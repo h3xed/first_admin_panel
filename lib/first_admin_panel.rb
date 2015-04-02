9aec73a15a6d05fb937286bdd2202b9540e8bef6
Alexander Borovykh <immaculate.pine@gmail.com>
2015-04-02 21:19:39 +0200

    Config added.

--- lib/first_admin_panel.rb
+++ lib/first_admin_panel.rb
@@ -1,4 +1,11 @@
 require "first_admin_panel/engine"
 
+require 'ostruct'
+
 module FirstAdminPanel
+  def self.config
+    @@config ||= OpenStruct.new
+  end
+
+  self.config.name = 'FirstAdminPanel'
 end
