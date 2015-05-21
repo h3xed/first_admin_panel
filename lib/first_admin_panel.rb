2c7047340f0f3fb5b6ea68cf19d9f6087484c4c6
Alexander Borovykh <immaculate.pine@gmail.com>
2015-05-21 15:41:31 +0200

    Config replaced by locales

--- lib/first_admin_panel.rb
+++ lib/first_admin_panel.rb
@@ -1,11 +1,4 @@
 require "first_admin_panel/engine"
 
-require 'ostruct'
-
 module FirstAdminPanel
-  def self.config
-    @@config ||= OpenStruct.new
-  end
-
-  self.config.name = 'FirstAdminPanel'
 end
